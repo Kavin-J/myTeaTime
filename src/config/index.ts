c8669cf453540e533da1f04385e6d9d04aa47fa9
Kavin24 <j.kavin2024@gmail.com>
2024-08-01 13:21:38 +0700

    git commit -m "Refactor: Reorganized project structure and created new configuration and source files

--- src/config/index.ts
+++
@@ -1,6 +0,0 @@
-import { config } from 'dotenv';
-config({path : `.env.${process.env.NODE_ENV}.local`});
-
-export const CREDENTIALS = process.env.CREDENTIALS === 'true';
-export const { NODE_ENV, PORT, SECRET_KEY, LOG_FORMAT, LOG_DIR, ORIGIN ,EXPIREIN} = process.env;
-export const { DB_HOST, DB_DATABASE , DB_USER , DB_PASSWORD , DB_OPTIONS } = process.env;
